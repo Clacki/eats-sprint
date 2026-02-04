84377804a293ea3378fbacdd953c15f4db27d1ad
Clacki <skthk6613@naver.com>
2026-02-04 18:29:28 +0900

    feat: #2 place-API 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 /** @format */
 
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
