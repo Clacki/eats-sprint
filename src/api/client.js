e8cf36df72b8a8cf9ad9ad65fff450d7095d8ec7
Clacki <skthk6613@naver.com>
2026-02-12 20:36:38 +0900

    last

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 /** @format */
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_API_BASE;
 
 export default async function fetchPlaces() {
   let res;
--- src/context/FavoritesProvider.jsx
+++ src/context/FavoritesProvider.jsx
@@ -2,7 +2,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
 const FavoritesContext = createContext(null);
-const API_BASE = "http://localhost:3000";
+const API_BASE = import.meta.env.VITE_API_BASE;
 
 export function FavoritesProvider({ children }) {
   const [favorites, setFavorites] = useState([]);
